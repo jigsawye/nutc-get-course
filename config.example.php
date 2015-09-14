a10a760137f26611bf234140179dddf04d9c42c6
jigsawye <jigsaw.ye@gmail.com>
2015-09-14 19:54:41 +0800

    first commit

---
+++ config.example.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'cookie' => 'your-cookie',
+    'raw' => 'your-rawdata',
+];
---
+++ root.php
@@ -0,0 +1,45 @@
+<?php
+require 'vendor/autoload.php';
+$config = require 'config.php';
+
+use GuzzleHttp\Client;
+
+$client = new Client;
+$url = 'https://ais.nutc.edu.tw/student/WebService.asmx/stuClassMod';
+$headers = [
+    'Content-Type' => 'application/json; charset=UTF-8',
+    'Cookie' => $config['cookie'],
+];
+$body = $config['raw'];
+
+$times = 0;
+
+function addCourse($client, $url, $headers, $body) {
+    $response = $client->request('post', $url, [
+        'headers' => $headers,
+        'body' => $body,
+    ]);
+
+    return $response;
+}
+
+function parseResponse($response) {
+    $resBody = (string) $response->getBody();
+    $resJson = json_decode($resBody);
+
+    $json_str = str_replace('\\', '', $resJson->d);
+    $result = json_decode($json_str);
+
+    return $result;
+}
+
+do {
+    $response = addCourse($client, $url, $headers, $body);
+    $result = parseResponse($response);
+    $message = $result[0]->args[0];
+    $state = $message == '課程人數已滿 !';
+
+    $times ++;
+
+    print "times: $times, message: $message\n";
+} while ($state);
